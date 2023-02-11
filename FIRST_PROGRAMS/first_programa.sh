4b00e4f4100b71690e61a690776a45fe15123f66
fsilva1984 <fsilva916@hotmail>
2023-02-11 04:00:46 +0000

    Elementary

---
+++ FIRST_PROGRAMS/first_programa.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+#  listUsr.sh - get users in /etc/passwd
+# 
+# site = https://github.com/fsilva1984/repo_shell 
+#
+# maintainer = Fsilva
+# autor = Fsilva
+# email = fsilva916@hotmail.com
+#-------------------------------------------------------------------#
+#
+# extrai usuarios de /etc/passwd  podendo ajustar em order
+# alfabetica e em letras maiusculas
+#
+#
+# exemplo:
+#  ./listUsr.sh -o -m
+#
+# neste exemplo teremos tudo em maiusculo e em orderm alfabetica
+#-------------------------------------------------------------------#
+#
+#    historico
+#
+# v1.0.0 / Fsilva
+# date 08/02/2023
+#
+#-------------------------------------------------------------------#
+#    agradecimentos 
+#
+#  Deus Senhor sobre tudo e todos
+#  
+######################################################################
+
---
+++ FUNCOES/get_params.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+function myfuncao
+{
+  # <codigo>
+  echo "hello, world !"
+}
+
+myfuncao
+
